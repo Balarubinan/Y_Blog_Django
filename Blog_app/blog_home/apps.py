eba9aed3548ae39e61617d160b0938e390b31921
Bala <sudhagouthirubi@gmail.com>
2021-05-29 17:08:22 -1200

    * added all the initial files * created models * code upto part-2 of the Django series
    Signed-off-by: Bala <sudhagouthirubi@gmail.com>

---
+++ Blog_app/blog_home/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogHomeConfig(AppConfig):
+    name = 'blog_home'
---
+++ Blog_app/blog_home/models.py
@@ -0,0 +1,20 @@
+from django.db.models import CharField,IntegerField,Model,BinaryField,DateField
+
+# Create your models here.
+class User(Model):
+    username=CharField(max_length=100)
+    password=CharField(max_length=100)
+    email=CharField(max_length=100)
+    phone=CharField(max_length=100)
+    profile_pic=BinaryField()
+
+class blog_table(Model):
+    blog_title=CharField(max_length=100)
+    blog_html=CharField(max_length=100) # denotes path to the html file of the blog
+    date=DateField()
+    media_id=CharField(max_length=100) # path to the media used
+
+class media_table(Model):
+    id=IntegerField()
+    data=BinaryField() # data => music,video,image
+
