e99a67a5c78b336947fb8e38adc23af7ed5bed72
Bala <sudhagouthirubi@gmail.com>
2021-06-13 18:07:36 -1200

    * updated till part - 7
    Signed-off-by: Bala <sudhagouthirubi@gmail.com>

--- Blog_app/blog_home/views.py
+++ Blog_app/blog_home/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render, redirect
 from django.template import Template,loader
 from django.http import HttpResponse
@@ -8,10 +10,12 @@ def show_blog_create(request):
     if request.method=="POST":
         print([x for x in request.POST])
         print(request.POST['text_editor'])
-        with open("title.html","w") as f:
+        title=request.POST['title']
+        content=request.POST['text_editor']
+        with open(f"{title}.html","w") as f:
             f.write(request.POST['text_editor'])
+        return redirect("/blog/adminpage")
     template=loader.get_template("blog_home/blog_create.html")
-
     return HttpResponse(template.render({},request))
 
 def show_dashboard(req):
